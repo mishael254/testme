33f8959d7bfa7dd93b9fb1dfd35c790a8b6a9953
mishael <mishaelmishantoz@gmail.com>
2024-04-27 16:58:08 +0300

    use the isset function to handle error when you first login into the page

--- 010_getandpostsuperG.php
+++ 010_getandpostsuperG.php
@@ -5,15 +5,24 @@
  * We can pass data through urls and forms
  * using the $_GET and $_POST superglobals
  */
-echo $_GET["name"];
-echo $_GET["age"]
+
+ if(isset($_POST["submit"])){
+    echo $_POST["name"];
+    echo $_POST["age"];
+ }
+;
 ?>
 
 <a href="<?php echo $_SERVER["PHP_SELF"]?>?name=john&age=25">Click</a>
-<form action="<?php echo $_SERVER["PHP_SELF"]?>">
+<form action="<?php echo $_SERVER["PHP_SELF"]?>" method="post" >
     <div>
         <label for="name">Name:</label>
         <input type="text" name="name">
     </div>
+    <div>
+        <label for="age">Your age:</label>
+        <input type="text" name="age">
+    </div>
+    <input type="submit" value="Submit" name="submit">
 
 </form>
